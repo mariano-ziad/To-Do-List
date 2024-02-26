146cacaefe90ce906ec8031384c6f96a0bfe36c9
ziad <ziadmoo1999@gmail.com>
2024-02-26 03:42:28 +0200

    to do list v1

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js")
+const port = 3000 || process.env.PORT;
+const tasks = [];
+const workTasks = [];
+// ------------------------------------------------------
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+// ------------------------------------------------------
+app.get("/", (req, res) => {
+    const day = date.getDate();
+    res.render("list", { listTitle: day, newTask: tasks});
+});
+
+app.post("/", (req, res) => {
+    const task = req.body.newItem;
+    if (req.body.list === "Work List") {
+        workTasks.push(task);
+        res.redirect("/work");
+    } else {
+        tasks.push(task);
+        res.redirect("/");
+    }
+});
+// ---------------------------------------------------------
+app.get("/work",(req, res)=>{
+    res.render("list",{listTitle: "Work List", newTask: workTasks});
+});
+// --------------------------------------------------------------
+app.listen(port, (req, res) => {
+    console.log("server started on port " + port);
+});
